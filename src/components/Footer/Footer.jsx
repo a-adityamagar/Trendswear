f972a6b0c2fee0f783ce88067336ee6fdca4ecd0
a-adityamagar <adityamagar677@gmail.com>
2024-12-02 15:46:43 +0545

    trendswear

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import footerLogo from "../../assets/logo.png";
+import Banner from "../../assets/website/footer-pattern.jpg";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaLocationArrow,
+  FaMobileAlt,
+} from "react-icons/fa";
+
+const BannerImg = {
+  backgroundImage: `url(${Banner})`,
+  backgroundPosition: "bottom",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  height: "100%",
+  width: "100%",
+};
+
+const FooterLinks = [
+  {
+    title: "Home",
+    link: "/#",
+  },
+  {
+    title: "About",
+    link: "/#about",
+  },
+  {
+    title: "Contact",
+    link: "/#contact",
+  },
+  {
+    title: "Blog",
+    link: "/#blog",
+  },
+];
+
+const Footer = () => {
+  return (
+    <div style={BannerImg} className="text-white">
+      <div className="container flex felx-col justify-evenly items-center">
+        <div data-aos="zoom-in" className="grid md:grid-cols-3 pb-44 pt-5">
+          {/* company details */}
+          <div className="py-8 px-4">
+            <h1 className="sm:text-3xl text-xl font-bold sm:text-left text-justify mb-3 flex items-center gap-3">
+              <img src={footerLogo} alt="" className="max-w-[50px]" />
+              TrendsWear
+            </h1>
+            <p>
+              Place where you can find the latest fashion trends and styles for men, women, and kids. We offer a wide range of clothing, accessories, and shoes for all occasions.
+            </p>
+          </div>
+
+          {/* Footer Links */}
+          <div className="grid grid-cols-2 sm:grid-cols-3 col-span-2 md:pl-10">
+
+            <div>
+              <div className="flex items-center gap-3 mt-6">
+                <a href="#">
+                  <FaInstagram className="text-3xl" />
+                </a>
+                <a href="#">
+                  <FaFacebook className="text-3xl" />
+                </a>
+                <a href="#">
+                  <FaLinkedin className="text-3xl" />
+                </a>
+              </div>
+              <div className="mt-6">
+                <div className="flex items-center gap-3">
+                  <FaLocationArrow />
+                  <p>Biratnagar, Nepal</p>
+                </div>
+                <div className="flex items-center gap-3 mt-3">
+                  <FaMobileAlt />
+                  <p>+977 98516616</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/TopProducts/TopProducts.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import Img1 from "../../assets/shirt/shirt.png";
+import Img2 from "../../assets/shirt/shirt2.png";
+import Img3 from "../../assets/shirt/shirt3.png";
+import { FaStar } from "react-icons/fa";
+
+const ProductsData = [
+  {
+    id: 1,
+    img: Img1,
+    title: "Casual Wear",
+    description:
+      "Plane shirt is a type of shirt that is typically made from a lightweight, breathable fabric such as cotton or linen. It is often worn as a casual or everyday shirt, and can be paired with a variety of bottoms, such as jeans, khakis, or shorts.",
+  },
+  {
+    id: 2,
+    img: Img2,
+    title: "Printed shirt",
+    description:
+      "Printed shirts are shirts that have been printed with a design or pattern on the fabric. These designs can be created using a variety of methods, such as screen printing, digital printing, or embroidery.",
+  },
+  {
+    id: 3,
+    img: Img3,
+    title: "Women shirt",
+    description:
+      "Women's shirts are shirts designed specifically for women. They come in a variety of styles, colors, and fabrics, and can be worn for both casual and formal occasions.",
+  },
+];
+const TopProducts = ({ handleOrderPopup }) => {
+  return (
+    <div>
+      <div className="container">
+        {/* Header section */}
+        <div className="text-left mb-24">
+          <p data-aos="fade-up" className="text-sm text-primary">
+            Top Rated Products for you
+          </p>
+          <h1 data-aos="fade-up" className="text-3xl font-bold">
+            Best Products
+          </h1>
+          <p data-aos="fade-up" className="text-xs text-gray-400">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit
+            asperiores modi Sit asperiores modi
+          </p>
+        </div>
+        {/* Body section */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-20 md:gap-5 place-items-center">
+          {ProductsData.map((data) => (
+            <div
+              data-aos="zoom-in"
+              className="rounded-2xl bg-white dark:bg-gray-800 hover:bg-black/80 dark:hover:bg-primary hover:text-white relative shadow-xl duration-300 group max-w-[300px]"
+            >
+              {/* image section */}
+              <div className="h-[100px]">
+                <img
+                  src={data.img}
+                  alt=""
+                  className="max-w-[140px] block mx-auto transform -translate-y-20 group-hover:scale-105 duration-300 drop-shadow-md"
+                />
+              </div>
+              {/* details section */}
+              <div className="p-4 text-center">
+                {/* star rating */}
+                <div className="w-full flex items-center justify-center gap-1">
+                  <FaStar className="text-yellow-500" />
+                  <FaStar className="text-yellow-500" />
+                  <FaStar className="text-yellow-500" />
+                  <FaStar className="text-yellow-500" />
+                </div>
+                <h1 className="text-xl font-bold">{data.title}</h1>
+                <p className="text-gray-500 group-hover:text-white duration-300 text-sm line-clamp-2">
+                  {data.description}
+                </p>
+                <button
+                  className="bg-primary hover:scale-105 duration-300 text-white py-1 px-4 rounded-full mt-4 group-hover:bg-white group-hover:text-primary"
+                  onClick={handleOrderPopup}
+                >
+                  Order Now
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TopProducts;
